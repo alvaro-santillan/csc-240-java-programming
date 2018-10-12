a73d3cf67357302b048863fa0ec8c31439af55df
Al <afsm1995@live.com>
2018-10-11 23:41:54 -0600

    Term Project Main Final Submit

---
+++ module-5-assignment-3–term-project/CSC 240 Project_Santillan/CSC 240 Project_Santillan/main.java
@@ -0,0 +1,20 @@
+package calculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class main extends Application {
+
+    public void start (Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("calculator_FXML.fxml"));
+        primaryStage.setTitle("Purple Calculator Classic");
+        primaryStage.setScene(new Scene(root, 390, 590));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main (String[] args) { launch(args); }
+}
